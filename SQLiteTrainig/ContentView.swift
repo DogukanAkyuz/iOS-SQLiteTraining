6c5beca11c3380f7c510520112367889f67cd975
DogukanAkyuz <sdogukanakyuz@gmail.com>
2024-12-05 16:19:07 +0300

    Initial Commit

---
+++ SQLiteTrainig/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  SQLiteTrainig
+//
+//  Created by Doğukan Akyüz on 5.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("SQLite")
+    }
+}
+
+#Preview {
+    ContentView() 
+}
---
+++ SQLiteTrainig/SQLiteTrainigApp.swift
@@ -0,0 +1,17 @@
+//
+//  SQLiteTrainigApp.swift
+//  SQLiteTrainig
+//
+//  Created by Doğukan Akyüz on 5.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SQLiteTrainigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
